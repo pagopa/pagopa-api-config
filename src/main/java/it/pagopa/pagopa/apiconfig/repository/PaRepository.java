90a38e9e416a26cfbf66ba1fe1bcd77a4afe180d
Jacopo Carlini <jacopo.carlini@gmail.com>
2021-09-10 17:56:36 +0200

    [PCFG-12] docker container improvements

---
+++ src/main/java/it/pagopa/pagopa/apiconfig/repository/PaRepository.java
@@ -0,0 +1,9 @@
+package it.pagopa.pagopa.apiconfig.repository;
+
+import it.pagopa.pagopa.apiconfig.models.Pa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaRepository extends JpaRepository<Pa, Long> {
+}
