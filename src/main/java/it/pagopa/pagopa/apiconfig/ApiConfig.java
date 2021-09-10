d621e17a7d242508674387603a66ca6c4da82230
Jacopo Carlini <jacopo.carlini@gmail.com>
2021-09-10 15:14:56 +0200

    code owner

---
+++ src/main/java/it/pagopa/pagopa/apiconfig/ApiConfig.java
@@ -0,0 +1,14 @@
+package it.pagopa.pagopa.apiconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class ApiConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiConfig.class, args);
+    }
+}
