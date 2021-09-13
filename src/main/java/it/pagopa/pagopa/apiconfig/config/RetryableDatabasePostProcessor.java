1c1412b34eafcfc64a38fbc53ea82fc8b8405e04
Jacopo Carlini <jacopo.carlini@gmail.com>
2021-09-13 15:15:02 +0200

    added retryable in s spring and wait-for-it script

---
+++ src/main/java/it/pagopa/pagopa/apiconfig/config/RetryableDatabasePostProcessor.java
@@ -0,0 +1,29 @@
+package it.pagopa.pagopa.apiconfig.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@Order(value = Ordered.HIGHEST_PRECEDENCE)
+@Component
+public class RetryableDatabasePostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if(bean instanceof DataSource dataSource) {
+            log.info("-----> configuring a retryable datasource for beanName = {}", beanName);
+            return new RetryableDataSource(dataSource);
+        }
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+}
