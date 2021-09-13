c17183f20a19297907f01f92223f31431dbecae8
Jacopo Carlini <jacopo.carlini@gmail.com>
2021-09-13 15:15:02 +0200

    added retryable in s spring and wait-for-it script

--- src/main/java/it/pagopa/pagopa/apiconfig/ApiConfig.java
+++ src/main/java/it/pagopa/pagopa/apiconfig/ApiConfig.java
@@ -3,9 +3,11 @@ package it.pagopa.pagopa.apiconfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableRetry
 public class ApiConfig {
 
     public static void main(String[] args) {
---
+++ src/main/java/it/pagopa/pagopa/apiconfig/config/RetryableDataSource.java
@@ -0,0 +1,33 @@
+package it.pagopa.pagopa.apiconfig.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.AbstractDataSource;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class RetryableDataSource extends AbstractDataSource {
+
+    private final DataSource dataSource;
+
+
+    @Override
+    @Retryable(maxAttempts = 10, backoff = @Backoff(delay = 8000, multiplier = 2, maxDelay = 600000))
+    public Connection getConnection() throws SQLException {
+        log.info("try getting connection...");
+        return dataSource.getConnection();
+    }
+
+    @Override
+    @Retryable(maxAttempts = 10, backoff = @Backoff(delay = 8000, multiplier = 2, maxDelay = 600000))
+    public Connection getConnection(String username, String password) throws SQLException {
+        log.info("try getting connection by username and password ...");
+        return dataSource.getConnection(username, password);
+    }
+}
