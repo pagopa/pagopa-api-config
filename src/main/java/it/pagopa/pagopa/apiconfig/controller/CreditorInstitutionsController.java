d7b7bc1545234f717a3810c1df596327830e1b20
Jacopo Carlini <jacopo.carlini@gmail.com>
2021-09-15 11:55:43 +0200

    [PCFG-16] new base-path

--- src/main/java/it/pagopa/pagopa/apiconfig/controller/CreditorInstitutionsController.java
+++ src/main/java/it/pagopa/pagopa/apiconfig/controller/CreditorInstitutionsController.java
@@ -12,9 +12,11 @@ import it.pagopa.pagopa.apiconfig.service.CreditorInstitutionsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/creditorinstitutions")
+@RestController()
+@RequestMapping(path = "/creditorinstitutions")
 public class CreditorInstitutionsController {
 
     @Autowired
