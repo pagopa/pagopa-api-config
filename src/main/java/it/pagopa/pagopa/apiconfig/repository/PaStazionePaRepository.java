1aa7f626e14a1405b36d646940205e7ca94d078a
Jacopo Carlini <jacopo.carlini@gmail.com>
2021-09-29 18:45:03 +0200

    [PCFG-30] return not found

--- src/main/java/it/pagopa/pagopa/apiconfig/repository/PaStazionePaRepository.java
+++ src/main/java/it/pagopa/pagopa/apiconfig/repository/PaStazionePaRepository.java
@@ -1,9 +1,7 @@
 package it.pagopa.pagopa.apiconfig.repository;
 
 import it.pagopa.pagopa.apiconfig.entity.PaStazionePa;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,9 +9,6 @@ import java.util.List;
 @Repository
 public interface PaStazionePaRepository extends PagingAndSortingRepository<PaStazionePa, Long> {
 
-
-    @Query(value = "select distinct r from PaStazionePa r join r.fkPa p join r.fkStazione s " +
-            "where (:creditorInstitutionCode is null or p.idDominio=:creditorInstitutionCode) ")
-    List<PaStazionePa> findAllFilterByPa(@Param("creditorInstitutionCode") String creditorInstitutionCode);
+    List<PaStazionePa> findAllByFkPa_ObjId(Long creditorInstitutionCode);
 
 }
