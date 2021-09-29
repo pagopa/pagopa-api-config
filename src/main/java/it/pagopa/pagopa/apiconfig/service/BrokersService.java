0702c7b294488322ad84cb24dbad60085882818f
Jacopo Carlini <jacopo.carlini@gmail.com>
2021-09-29 18:25:23 +0200

    [PCFG-15] done service for details

--- src/main/java/it/pagopa/pagopa/apiconfig/service/BrokersService.java
+++ src/main/java/it/pagopa/pagopa/apiconfig/service/BrokersService.java
@@ -1,7 +1,9 @@
 package it.pagopa.pagopa.apiconfig.service;
 
 import it.pagopa.pagopa.apiconfig.entity.IntermediariPa;
+import it.pagopa.pagopa.apiconfig.exception.AppException;
 import it.pagopa.pagopa.apiconfig.model.Broker;
+import it.pagopa.pagopa.apiconfig.model.BrokerDetails;
 import it.pagopa.pagopa.apiconfig.model.Brokers;
 import it.pagopa.pagopa.apiconfig.repository.IntermediariPaRepository;
 import it.pagopa.pagopa.apiconfig.util.CommonUtil;
@@ -10,10 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -34,9 +38,26 @@ public class BrokersService {
                 .build();
     }
 
+    public BrokerDetails getBroker(String brokerCode) {
+        Optional<IntermediariPa> result = intermediariPaRepository.findByCodiceIntermediario(brokerCode);
+        if (result.isEmpty()) {
+            throw new AppException(HttpStatus.NOT_FOUND, "Broker not found", "No broker found with the provided code");
+        }
+        IntermediariPa intermediariPa = result.get();
+        return modelMapper.map(intermediariPa, BrokerDetails.class);
+    }
+
+    /**
+     * Maps IntermediariPa objects stored in the DB in a List of Broker
+     *
+     * @param page page of {@link IntermediariPa} returned from the database
+     * @return a list of {@link Broker}.
+     */
     private List<Broker> getBrokerList(Page<IntermediariPa> page) {
         return page.stream()
                 .map(elem -> modelMapper.map(elem, Broker.class))
                 .collect(Collectors.toList());
     }
+
+
 }
