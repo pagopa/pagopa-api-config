364ee815703be898d923e8c5bba2b00300bcfbca
Jacopo Carlini <jacopo.carlini@gmail.com>
2021-09-29 18:09:44 +0200

    [PCFG-30] used modelMapper

--- src/main/java/it/pagopa/pagopa/apiconfig/repository/IbanValidiPerPaRepository.java
+++ src/main/java/it/pagopa/pagopa/apiconfig/repository/IbanValidiPerPaRepository.java
@@ -3,9 +3,11 @@ package it.pagopa.pagopa.apiconfig.repository;
 import it.pagopa.pagopa.apiconfig.entity.IbanValidiPerPa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface IbanValidiPerPaRepository extends JpaRepository<IbanValidiPerPa, String> {
 
     @Query("select i from IbanValidiPerPa i, Pa p where p.idDominio=:creditorInstitutionCode and p.objId=i.fkPa")
