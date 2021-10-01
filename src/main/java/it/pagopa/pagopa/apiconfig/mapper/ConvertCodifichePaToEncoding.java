67a4d028234738a910d64e77117f8110aca4682c
Jacopo Carlini <jacopo.carlini@gmail.com>
2021-10-01 16:45:11 +0200

    [PCFG-15] rebase

---
+++ src/main/java/it/pagopa/pagopa/apiconfig/mapper/ConvertCodifichePaToEncoding.java
@@ -0,0 +1,34 @@
+package it.pagopa.pagopa.apiconfig.mapper;
+
+import it.pagopa.pagopa.apiconfig.entity.CodifichePa;
+import it.pagopa.pagopa.apiconfig.model.Encoding;
+import org.modelmapper.Converter;
+import org.modelmapper.spi.MappingContext;
+
+import javax.validation.Valid;
+
+public class ConvertCodifichePaToEncoding implements Converter<CodifichePa, Encoding> {
+
+    @Override
+    public Encoding convert(MappingContext<CodifichePa, Encoding> context) {
+        @Valid CodifichePa source = context.getSource();
+        return Encoding.builder()
+                .codeType(getCodeType(source))
+                .code(source.getCodicePa())
+                .build();
+    }
+
+    /**
+     * Null-safe conversion
+     *
+     * @param source {@link CodifichePa}
+     * @return {@link Encoding.CodeTypeEnum}
+     */
+    private Encoding.CodeTypeEnum getCodeType(CodifichePa source) {
+        if (source != null && source.getFkCodifica() != null) {
+            return Encoding.CodeTypeEnum.fromValue(source.getFkCodifica().getIdCodifica());
+        } else {
+            return null;
+        }
+    }
+}
