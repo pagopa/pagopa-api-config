8069e709a973675ae5f243a23ba560ca2c1607e9
Jacopo Carlini <jacopo.carlini@gmail.com>
2021-10-04 12:55:57 +0200

    [PCFG-46] added timeout before integration tests

--- src/main/java/it/pagopa/pagopa/apiconfig/controller/HomeController.java
+++ src/main/java/it/pagopa/pagopa/apiconfig/controller/HomeController.java
@@ -1,6 +1,13 @@
 package it.pagopa.pagopa.apiconfig.controller;
 
 import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import it.pagopa.pagopa.apiconfig.model.ProblemJson;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +31,13 @@ public class HomeController {
      *
      * @return ok
      */
-    @Hidden
+    @Operation(summary = "Return OK if application is started", security = {@SecurityRequirement(name = "ApiKey")}, tags = {"Home"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK."),
+            @ApiResponse(responseCode = "403", description = "Forbidden client error status.", content = @Content(mediaType = "application/json", schema = @Schema())),
+            @ApiResponse(responseCode = "429", description = "Too many requests", content = @Content(mediaType = "application/json", schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Service unavailable.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProblemJson.class)))})
+
     @GetMapping("/info")
     public ResponseEntity<Void> healthCheck() {
         return ResponseEntity.ok().build();
