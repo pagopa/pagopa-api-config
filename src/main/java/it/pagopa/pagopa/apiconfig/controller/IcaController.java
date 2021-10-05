5367e19fe8b4f606a146d6c4ba1bb00940dead0f
Jacopo Carlini <jacopo.carlini@gmail.com>
2021-10-05 12:40:06 +0200

    [PCFG-28] implemented business logic - updated swagger - updated collection for int. tests

---
+++ src/main/java/it/pagopa/pagopa/apiconfig/controller/IcaController.java
@@ -0,0 +1,73 @@
+package it.pagopa.pagopa.apiconfig.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import it.pagopa.pagopa.apiconfig.model.Icas;
+import it.pagopa.pagopa.apiconfig.model.ProblemJson;
+import it.pagopa.pagopa.apiconfig.service.IcaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Positive;
+
+@RestController()
+@RequestMapping(path = "/icas")
+@Tag(name = "Creditor Institutions", description = "Everything about Creditor Institution")
+public class IcaController {
+
+    @Autowired
+    IcaService icaService;
+
+
+    @Operation(summary = "Get the list of ICAs", security = {@SecurityRequirement(name = "ApiKey")}, tags = {"Creditor Institutions",})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Icas.class))),
+            @ApiResponse(responseCode = "403", description = "Forbidden client error status.", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "429", description = "Too many requests", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Service unavailable.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProblemJson.class)))})
+    @GetMapping(
+            value = "",
+            produces = {"application/json"}
+    )
+    public ResponseEntity<Icas> getIcas(
+            @Positive @Parameter(description = "Number of elements on one page. Default = 50") @RequestParam(required = false, defaultValue = "50") Integer limit,
+            @Positive @Parameter(description = "Page number. Page value starts from 0", required = true) @RequestParam Integer page) {
+        return ResponseEntity.ok(icaService.getIcas(limit, page));
+    }
+
+
+    @Operation(summary = "Download a XML file containing the details of an ICA", security = {@SecurityRequirement(name = "ApiKey")}, tags = {"Creditor Institutions",})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK.", content = @Content(schema = @Schema(implementation = Resource.class))),
+            @ApiResponse(responseCode = "403", description = "Forbidden client error status.", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "429", description = "Too many requests", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Service unavailable.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProblemJson.class)))})
+    @GetMapping(
+            value = "/{idica}",
+            produces = {"application/xml"}
+    )
+    public ResponseEntity<Resource> getIca(@Parameter(description = "Id ICA", required = true) @PathVariable("idica") String idIca) {
+        byte[] file = icaService.getIca(idIca);
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_XML)
+                .contentLength(file.length)
+                .body(new ByteArrayResource(file));
+    }
+
+
+}
