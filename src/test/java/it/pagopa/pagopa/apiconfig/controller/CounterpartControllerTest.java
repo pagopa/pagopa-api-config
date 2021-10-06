f61dcd7997116eaa21bc1d394fd3788726299f32
Jacopo Carlini <jacopo.carlini@gmail.com>
2021-10-06 10:45:13 +0200

    [PCFG-29] added unit tests

---
+++ src/test/java/it/pagopa/pagopa/apiconfig/controller/CounterpartControllerTest.java
@@ -0,0 +1,52 @@
+package it.pagopa.pagopa.apiconfig.controller;
+
+import it.pagopa.pagopa.apiconfig.ApiConfig;
+import it.pagopa.pagopa.apiconfig.model.CounterpartTables;
+import it.pagopa.pagopa.apiconfig.service.CounterpartService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = ApiConfig.class)
+@AutoConfigureMockMvc
+class CounterpartControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private CounterpartService counterpartService;
+
+    @BeforeEach
+    void setUp() {
+        when(counterpartService.getCounterpartTables(50, 0)).thenReturn(CounterpartTables.builder().build());
+        when(counterpartService.getCounterpartTable(anyString(), anyString())).thenReturn(new byte[]{});
+    }
+
+    @Test
+    void getCounterpartTables() throws Exception {
+        String url = "/counterparttables?page=0";
+        mvc.perform(get(url).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void getCounterpartTable() throws Exception {
+        String url = "/counterparttables/123?creditorinstitutioncode=111";
+        mvc.perform(get(url).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_XML));
+    }
+}
---
+++ src/test/java/it/pagopa/pagopa/apiconfig/service/CounterpartServiceTest.java
@@ -0,0 +1,60 @@
+package it.pagopa.pagopa.apiconfig.service;
+
+import it.pagopa.pagopa.apiconfig.ApiConfig;
+import it.pagopa.pagopa.apiconfig.TestUtil;
+import it.pagopa.pagopa.apiconfig.entity.InformativePaMaster;
+import it.pagopa.pagopa.apiconfig.model.CounterpartTables;
+import it.pagopa.pagopa.apiconfig.repository.InformativePaMasterRepository;
+import org.assertj.core.util.Lists;
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static it.pagopa.pagopa.apiconfig.TestUtil.getMockInformativePaMaster;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(classes = ApiConfig.class)
+class CounterpartServiceTest {
+
+    @MockBean
+    private InformativePaMasterRepository informativePaMasterRepository;
+
+    @Autowired
+    @InjectMocks
+    private CounterpartService counterpartService;
+
+    @Test
+    void getCounterpartTables() throws IOException, JSONException {
+        Page<InformativePaMaster> page = TestUtil.mockPage(Lists.newArrayList(getMockInformativePaMaster()), 50, 0);
+        when(informativePaMasterRepository.findAll(any(Pageable.class))).thenReturn(page);
+
+        CounterpartTables result = counterpartService.getCounterpartTables(50, 0);
+        String actual = TestUtil.toJson(result);
+        String expected = TestUtil.readJsonFromFile("response/get_counterparttables_ok.json");
+        System.out.println(actual);
+        JSONAssert.assertEquals(expected, actual, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    void getCounterpartTable() {
+        when(informativePaMasterRepository.findByIdInformativaPaAndFkPa_IdDominio(anyString(), anyString())).thenReturn(Optional.of(getMockInformativePaMaster()));
+
+        byte[] result = counterpartService.getCounterpartTable("111", "222");
+        assertNotNull(result);
+        assertEquals(2, result.length);
+    }
+}
