e77aae76f57b8bc7c179ef095809cc110baf302b
Jacopo Carlini <jacopo.carlini@gmail.com>
2021-10-19 17:14:39 +0200

    [PCFG-34] disable CORS

--- src/main/java/it/pagopa/pagopa/apiconfig/config/WebMvcConfiguration.java
+++ src/main/java/it/pagopa/pagopa/apiconfig/config/WebMvcConfiguration.java
@@ -2,25 +2,39 @@ package it.pagopa.pagopa.apiconfig.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 import java.util.UUID;
 
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
+    @Autowired
+    private Environment environment;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new MDCInterceptor());
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // disable CORS only in local environments
+        if (!Arrays.asList(environment.getActiveProfiles()).isEmpty()) {
+            registry.addMapping("/**").allowedMethods("*");
+        }
+    }
 
     @Slf4j
     public static class MDCInterceptor implements HandlerInterceptor {
