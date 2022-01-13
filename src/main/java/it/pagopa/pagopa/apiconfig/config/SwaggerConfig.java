6384f99de0854939db6ead3e6eb88c163f0021b8
Jacopo <jacopo.carlini@gmail.com>
2022-01-13 11:12:07 +0100

    added request-id in the swagger

--- src/main/java/it/pagopa/pagopa/apiconfig/config/SwaggerConfig.java
+++ src/main/java/it/pagopa/pagopa/apiconfig/config/SwaggerConfig.java
@@ -3,7 +3,10 @@ package it.pagopa.pagopa.apiconfig.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Paths;
+import io.swagger.v3.oas.models.headers.Header;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +14,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Map;
+
+import static it.pagopa.pagopa.apiconfig.util.Constants.HEADER_REQUEST_ID;
 
 @Configuration
 public class SwaggerConfig {
@@ -51,6 +56,25 @@ public class SwaggerConfig {
 
             openApi.setPaths(paths);
         };
+    }
+
+    @Bean
+    public OpenApiCustomiser addCommonHeaders() {
+        return openApi -> openApi.getPaths().forEach((key, value) -> {
+
+            // add Request-ID as request header
+            value.addParametersItem(new Parameter().in("header")
+                    .name(HEADER_REQUEST_ID)
+                    .description("This header identifies the call, if not passed it is self-generated. This ID is returned in the response."));
+
+            // add Request-ID as response header
+            value.readOperations()
+                    .forEach(operation -> operation.getResponses().values()
+                            .forEach(response -> response.addHeaderObject(HEADER_REQUEST_ID, new Header()
+                                    .schema(new StringSchema())
+                                    .required(true)
+                                    .description("This header identifies the call"))));
+        });
     }
 
 
