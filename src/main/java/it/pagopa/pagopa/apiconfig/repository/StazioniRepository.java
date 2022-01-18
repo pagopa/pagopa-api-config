591049aeaa36121c946342c7de9ef2117e1b1dce
Jacopo <jacopo.carlini@gmail.com>
2022-01-18 12:34:11 +0100

    remove commented code

--- src/main/java/it/pagopa/pagopa/apiconfig/repository/StazioniRepository.java
+++ src/main/java/it/pagopa/pagopa/apiconfig/repository/StazioniRepository.java
@@ -19,11 +19,6 @@ public interface StazioniRepository extends PagingAndSortingRepository<Stazioni,
             "where s.fkIntermediarioPa = i " +
             "and (:brokerCode is null or i.idIntermediarioPa = :brokerCode) " +
             "and (:creditorInstitutionCode is null or (p.idDominio = :creditorInstitutionCode and r.fkPa = p and r.fkStazione = s))")
-//    select distinct s.*
-//    from STAZIONI s, PA_STAZIONE_PA r, INTERMEDIARI_PA i, PA p
-//    where s.FK_INTERMEDIARIO_PA = i.OBJ_ID
-//    and (?1 is null or i.ID_INTERMEDIARIO_PA = ?1)
-//    and (?2 is null or (p.ID_DOMINIO = ?2 and r.FK_PA = p.OBJ_ID and r.FK_STAZIONE = s.OBJ_ID))
     Page<Stazioni> findAllFilterByIntermediarioAndPa(@Param("brokerCode") String brokerCode, @Param("creditorInstitutionCode") String creditorInstitutionCode, Pageable pageable);
 
 }
