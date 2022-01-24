d23c5bde280f1b4da9ce31348d688f27506a1a97
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-01-24 08:35:09 +0100

    minor fix

--- src/main/java/it/pagopa/pagopa/apiconfig/controller/ConfigurationController.java
+++ src/main/java/it/pagopa/pagopa/apiconfig/controller/ConfigurationController.java
@@ -216,7 +216,7 @@ public class ConfigurationController {
     @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class))),
     @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema())),
     @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema())),
-    @ApiResponse(responseCode = "409", description = "Conflict", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class))),
+    @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class))),
     @ApiResponse(responseCode = "429", description = "Too many requests", content = @Content(schema = @Schema())),
     @ApiResponse(responseCode = "500", description = "Service unavailable", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class)))})
     @PutMapping(value = "/wfespplugins/{idServPlugin}", produces = {MediaType.APPLICATION_JSON_VALUE})
