115bf6d93605b23ec346927590878a0b44e4e05c
Jacopo <jacopo.carlini@gmail.com>
2022-01-31 15:26:47 +0100

    PCFG-145 added log execution time

--- src/main/java/it/pagopa/pagopa/apiconfig/model/AppInfo.java
+++ src/main/java/it/pagopa/pagopa/apiconfig/model/AppInfo.java
@@ -2,12 +2,15 @@ package it.pagopa.pagopa.apiconfig.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+
+import javax.validation.constraints.NotNull;
 
 @Data
 @Builder(toBuilder = true)
@@ -16,9 +19,14 @@ import lombok.ToString;
 @ToString
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Schema(required = true)
+@NotNull
 public class AppInfo {
 
+    @NotNull
     private String name;
+    @NotNull
     private String version;
+    @NotNull
     private String environment;
 }
