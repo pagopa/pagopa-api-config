7d6ff31e8655c6b61b0b1513153ca5afa94a8b9b
Jacopo <jacopo.carlini@gmail.com>
2022-02-04 09:31:58 +0100

    [PCFG-37] delete ica service

--- src/main/java/it/pagopa/pagopa/apiconfig/repository/InformativeContoAccreditoMasterRepository.java
+++ src/main/java/it/pagopa/pagopa/apiconfig/repository/InformativeContoAccreditoMasterRepository.java
@@ -4,6 +4,8 @@ import it.pagopa.pagopa.apiconfig.entity.InformativeContoAccreditoMaster;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
+import java.util.List;
 import java.util.Optional;
 
 @SuppressWarnings("java:S100") // Disabled naming convention rule for method name to use Spring Data interface
@@ -12,4 +14,5 @@ public interface InformativeContoAccreditoMasterRepository extends PagingAndSort
 
     Optional<InformativeContoAccreditoMaster> findByIdInformativaContoAccreditoPaAndFkPa_IdDominio(String idIca, String creditorInstitutionCode);
 
+    List<InformativeContoAccreditoMaster> findByFkPa_IdDominioAndDataInizioValiditaLessThanOrderByDataInizioValiditaDesc(String idDominio, Timestamp now);
 }
