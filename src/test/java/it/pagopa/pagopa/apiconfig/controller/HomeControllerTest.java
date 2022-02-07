9c5f79474626ff26c1c9b1f80c00d2362184abb3
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-02-07 15:34:38 +0100

    clean unuseful code

--- src/test/java/it/pagopa/pagopa/apiconfig/controller/HomeControllerTest.java
+++ src/test/java/it/pagopa/pagopa/apiconfig/controller/HomeControllerTest.java
@@ -1,25 +1,15 @@
 package it.pagopa.pagopa.apiconfig.controller;
 
 import it.pagopa.pagopa.apiconfig.ApiConfig;
-import it.pagopa.pagopa.apiconfig.TestUtil;
-import it.pagopa.pagopa.apiconfig.model.creditorinstitution.BrokerDetails;
-import it.pagopa.pagopa.apiconfig.model.filterandorder.FilterAndOrder;
-import it.pagopa.pagopa.apiconfig.repository.HealthCheckRepository;
-import it.pagopa.pagopa.apiconfig.service.BrokersService;
 import it.pagopa.pagopa.apiconfig.service.HealthCheckService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static it.pagopa.pagopa.apiconfig.TestUtil.getMockBrokerDetails;
-import static it.pagopa.pagopa.apiconfig.TestUtil.getMockBrokers;
-import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
