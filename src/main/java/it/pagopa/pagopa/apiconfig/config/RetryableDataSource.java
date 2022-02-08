60c39adf233fa9b07aa9be8454f8f6e38b971dbc
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-02-08 11:42:43 +0100

    reduced getConnection maxAttempts and hikariCP connection timeout

--- src/main/java/it/pagopa/pagopa/apiconfig/config/RetryableDataSource.java
+++ src/main/java/it/pagopa/pagopa/apiconfig/config/RetryableDataSource.java
@@ -3,7 +3,6 @@ package it.pagopa.pagopa.apiconfig.config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.AbstractDataSource;
-import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 
 import javax.sql.DataSource;
@@ -18,14 +17,14 @@ public class RetryableDataSource extends AbstractDataSource {
 
 
     @Override
-    @Retryable(maxAttempts = 10, backoff = @Backoff(delay = 8000, multiplier = 2, maxDelay = 600000))
+    @Retryable(maxAttempts = 1)
     public Connection getConnection() throws SQLException {
         log.debug("Trying to connect to the database...");
         return dataSource.getConnection();
     }
 
     @Override
-    @Retryable(maxAttempts = 10, backoff = @Backoff(delay = 8000, multiplier = 2, maxDelay = 600000))
+    @Retryable(maxAttempts = 1)
     public Connection getConnection(String username, String password) throws SQLException {
         log.debug("Trying to connect to the database by username and password ...");
         return dataSource.getConnection(username, password);
