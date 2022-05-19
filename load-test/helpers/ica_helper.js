3b17c89aced05d402813ada6a817f284a555513d
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-05-19 17:12:24 +0200

    [PCFG-210] ICA load test (#104)
    * ica load test
    
    * minor fix
    
    * minor fix

---
+++ load-test/helpers/ica_helper.js
@@ -0,0 +1,40 @@
+import http from 'k6/http';
+
+export function getIcas(rootUrl, params) {
+	const url = `${rootUrl}/icas?limit=50&page=0`
+	return http.get(url, params);
+}
+
+export function createIca(rootUrl, params, id, binFile) {
+	const url = `${rootUrl}/icas`
+
+	const payload = {
+		field: '',
+		file: http.file(binFile, `ica_${id}.xml`),
+	};
+
+	return http.post(url, payload);
+}
+
+export function getIca(rootUrl, params, id, ciId) {
+	const url = `${rootUrl}/icas/${ciId}_ica?creditorinstitutioncode=${ciId}`
+
+	return http.get(url, params);
+}
+
+export function deleteIca(rootUrl, params, id, ciId) {
+	const url = `${rootUrl}/icas/${ciId}_ica?creditorinstitutioncode=${ciId}`
+
+	return http.del(url, params);
+}
+
+export function checkXSD(rootUrl, params, id, binFile) {
+	const url = `${rootUrl}/icas/xsd`
+
+	const payload = {
+		field: '',
+		file: http.file(binFile, `ica_${id}.xml`),
+	};
+
+	return http.post(url, payload);
+}
---
+++ load-test/ica.js
@@ -0,0 +1,151 @@
+// 1. init code (once per VU)
+// prepares the script: loading files, importing modules, and defining functions
+
+import { check } from 'k6';
+import { SharedArray } from 'k6/data';
+
+import {getJWTToken} from "./helpers/authentication.js";
+
+import {
+	getIcas,
+	createIca,
+	getIca,
+	deleteIca,
+	checkXSD
+} from "./helpers/ica_helper.js";
+import {
+	getCiCode,
+	createCreditorInstitution,
+	deleteCreditorInstitution,
+	createEncodings,
+	deleteEncodings
+} from "./helpers/creditor_institutions_helper.js";
+
+
+// read configuration
+// note: SharedArray can currently only be constructed inside init code
+// according to https://k6.io/docs/javascript-api/k6-data/sharedarray
+const varsArray = new SharedArray('vars', function () {
+	return JSON.parse(open(`./${__ENV.VARS}`)).environment;
+});
+// workaround to use shared array (only array should be used)
+const vars = varsArray[0];
+const rootUrl = `${vars.host}/${vars.basePath}`;
+
+// open function is only available in the init stage
+const binFile = open('./resources/ica.xml');
+
+export function setup() {
+	// 2. setup code (once)
+	// The setup code runs, setting up the test environment (optional) and generating data
+	// used to reuse code for the same VU
+
+	// precondition is moved to default fn because in this stage
+	// __VU is always 0 and cannot be used to create env properly
+}
+
+function precondition(params, id) {
+	// pattern '[0-9]{11}' for type 'stCF'
+	const tempId = `9${id}`;
+
+	// remove ica if it already exists
+	let response = deleteIca(rootUrl, params, id, getCiCode(tempId));
+	let key = `initial step for ica ${getCiCode(tempId)}`;
+	check(response, {
+		[key]: (r) => r.status === 200 || r.status === 404,
+	});
+
+	// Create creditor institution
+	response = createCreditorInstitution(rootUrl, params, tempId);
+	check(response, {
+		'createCreditorInstitution': (r) => r.status === 201 || r.status === 409,
+	});
+
+	response = createEncodings(rootUrl, params, tempId);
+	check(response, {
+		'createEncodings': (r) => r.status === 201 || r.status === 409,
+	});
+}
+
+function postcondition(params, id) {
+	// pattern '[0-9]{11}' for type 'stCF'
+	const tempId = `9${id}`;
+
+	// remove creditor institution and encodings used in the test
+	let response = deleteEncodings(rootUrl, params, tempId);
+	let key = `final step for ica 1/2 ${getCiCode(tempId)}`;
+	check(response, {
+		[key]: (r) => r.status === 200 || r.status === 404,
+	});
+
+	response = deleteCreditorInstitution(rootUrl, params, tempId);
+	key = `final step for ica 2/2 ${getCiCode(tempId)}`;
+	check(response, {
+		[key]: (r) => r.status === 200 || r.status === 404,
+	});
+}
+
+export default function (data) {
+	// 3. VU code (once per iteration, as many times as the test options require)
+	// VU code runs in the default() function, running for as long and as many times as the options define.
+	const token = vars.env === "local" ? "-" : getJWTToken(vars.tenantId, vars.clientId, vars.clientSecret, vars.resource);
+
+	const params = {
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${token}`
+		},
+	};
+
+	precondition(params, __VU);
+
+	// pattern '[0-9]{11}' for type 'stCF'
+	const tempId = `9${__VU}`;
+
+	// Get icas
+	let response = getIcas(rootUrl, params);
+	check(response, {
+		'getIcas': (r) => r.status === 200,
+	});
+
+	// Create ica
+	const multipart_params = {
+		headers: {
+			'Content-Type': 'multipart/form-data',
+			'Authorization': `Bearer ${token}`
+		}
+	}
+	const replacedFile = binFile.replace(/\{creditor_institution}/g, getCiCode(tempId));
+	response = createIca(rootUrl, multipart_params, __VU, replacedFile);
+	check(response, {
+		'createIca': (r) => r.status === 201,
+	});
+
+	// Get ica
+	response = getIca(rootUrl, params, __VU, getCiCode(tempId));
+	check(response, {
+		'getIca': (r) => r.status === 200,
+	});
+
+	// Delete ica
+	response = deleteIca(rootUrl, params, __VU, getCiCode(tempId));
+	check(response, {
+		'deleteIca': (r) => r.status === 200,
+	});
+
+	response = checkXSD(rootUrl, multipart_params, __VU, replacedFile);
+	check(response, {
+		'checkXSD': (r) => r.status === 200,
+	});
+
+	postcondition(params, __VU);
+}
+
+export function teardown(data) {
+	// 4. teardown code (once per script)
+	// The teardown code runs, postprocessing data and closing the test environment.
+
+	// postcondition is moved to default fn because in this stage
+	// __VU is always 0 and cannot be used to create env properly
+
+}
