0fbc1810e00e90277be38f931d6af1c64af7132a
Jacopo <jacopo.carlini@gmail.com>
2022-08-22 18:36:52 +0200

    fix

--- database/sql/V13__ALTER_STAZIONI.sql
+++ database/sql/V13__ALTER_STAZIONI.sql
@@ -1,1 +1,1 @@
-alter table NODO4_CFG.STAZIONI add column if not exists INVIO_RT_ISTANTANEO CHAR default 'N' not null
+alter table NODO4_CFG.STAZIONI add INVIO_RT_ISTANTANEO CHAR default 'N' not null
