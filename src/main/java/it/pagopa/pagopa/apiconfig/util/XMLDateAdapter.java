00ceac3e003fc4dab31af0f39bccd20d3303d461
Jacopo Carlini <jacopo.carlini@gmail.com>
2022-10-20 16:29:03 +0200

    Update XMLDateAdapter.java

--- src/main/java/it/pagopa/pagopa/apiconfig/util/XMLDateAdapter.java
+++ src/main/java/it/pagopa/pagopa/apiconfig/util/XMLDateAdapter.java
@@ -6,16 +6,16 @@ import java.time.format.DateTimeFormatter;
 
 public class XMLDateAdapter extends XmlAdapter<String, LocalDateTime> {
 
-    private static final String pattern = "yyyy-MM-dd'T'HH:mm:ss";
+    private static final String PATTERN = "yyyy-MM-dd'T'HH:mm:ss";
 
     @Override
     public String marshal(LocalDateTime v) {
-        return v.format(DateTimeFormatter.ofPattern(pattern));
+        return v.format(DateTimeFormatter.ofPattern(PATTERN));
     }
 
     @Override
     public LocalDateTime unmarshal(String v) {
-        return LocalDateTime.parse(v, DateTimeFormatter.ofPattern(pattern));
+        return LocalDateTime.parse(v, DateTimeFormatter.ofPattern(PATTERN));
     }
 
 }
