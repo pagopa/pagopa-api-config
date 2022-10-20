b55af71626543043dc84ada4f168b96493fdfb67
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-10-20 10:13:46 +0200

    fix

--- src/main/java/it/pagopa/pagopa/apiconfig/util/XMLDateAdapter.java
+++ src/main/java/it/pagopa/pagopa/apiconfig/util/XMLDateAdapter.java
@@ -1,8 +1,6 @@
 package it.pagopa.pagopa.apiconfig.util;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -10,21 +8,13 @@ public class XMLDateAdapter extends XmlAdapter<String, LocalDateTime> {
 
     private static final String pattern = "yyyy-MM-dd'T'HH:mm:ss";
 
-    private static final ThreadLocal<DateFormat> dateFormat = new ThreadLocal<DateFormat>() {
-
-        @Override
-        protected DateFormat initialValue() {
-            return new SimpleDateFormat(pattern);
-        }
-    };
-
     @Override
-    public String marshal(LocalDateTime v) throws Exception {
-        return dateFormat.get().format(v);
+    public String marshal(LocalDateTime v) {
+        return v.format(DateTimeFormatter.ofPattern(pattern));
     }
 
     @Override
-    public LocalDateTime unmarshal(String v) throws Exception {
+    public LocalDateTime unmarshal(String v) {
         return LocalDateTime.parse(v, DateTimeFormatter.ofPattern(pattern));
     }
 
