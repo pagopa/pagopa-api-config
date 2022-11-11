5a4d3ee776cee42f4686dcf51114870ac9c83a18
Jacopo <jacopo.carlini@gmail.com>
2022-11-11 14:41:51 +0100

    improves some stuff

--- src/main/java/it/pagopa/pagopa/apiconfig/config/ResponseValidator.java
+++ src/main/java/it/pagopa/pagopa/apiconfig/config/ResponseValidator.java
@@ -1,29 +1,26 @@
 package it.pagopa.pagopa.apiconfig.config;
 
-import lombok.extern.slf4j.Slf4j;
+import it.pagopa.pagopa.apiconfig.exception.AppException;
 import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 import java.util.Set;
 
 @Aspect
 @Component
-@Slf4j
 public class ResponseValidator {
 
     @Autowired
     private Validator validator;
 
-    @Autowired
-    private HttpServletResponse response;
 
     /**
      * This method validates the response annotated with the {@link javax.validation.constraints}
@@ -38,9 +35,9 @@ public class ResponseValidator {
         }
     }
 
-    private <T> void validateResponse(ResponseEntity<T> responseEntity) {
-        if (responseEntity.getBody() != null) {
-            Set<ConstraintViolation<T>> validationResults = validator.validate(responseEntity.getBody());
+    private <T> void validateResponse(ResponseEntity<T> response) {
+        if (response.getBody() != null) {
+            Set<ConstraintViolation<T>> validationResults = validator.validate(response.getBody());
 
             if (!validationResults.isEmpty()) {
                 var sb = new StringBuilder();
@@ -48,8 +45,7 @@ public class ResponseValidator {
                     sb.append(error.getPropertyPath()).append(" ").append(error.getMessage()).append(". ");
                 }
                 var msg = StringUtils.chop(sb.toString());
-                log.warn("Invalid Response: {}", msg);
-                response.setHeader("X-Warning", msg);
+                throw new AppException(HttpStatus.INTERNAL_SERVER_ERROR, "Invalid response", msg);
             }
         }
     }
