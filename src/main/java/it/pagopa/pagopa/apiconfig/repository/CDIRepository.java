123499543896d4d6b78141d415b292080d527db7
Jacopo <jacopo.carlini@gmail.com>
2022-12-01 12:44:19 +0100

    tmp

---
+++ src/main/java/it/pagopa/pagopa/apiconfig/repository/CDIRepository.java
@@ -0,0 +1,7 @@
+//package it.pagopa.pagopa.apiconfig.repository;
+//
+//import com.azure.spring.data.cosmos.repository.CosmosRepository;
+//import it.pagopa.pagopa.apiconfig.entity.CDI;
+//
+//public interface CDIRepository extends CosmosRepository<CDI, String> {
+//}
