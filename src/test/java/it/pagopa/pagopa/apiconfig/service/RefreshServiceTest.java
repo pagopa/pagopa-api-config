eccefc38cf3a9dbf370707b71bd24484f1958056
AngeloCaporaso <angelo.caporaso@pagopa.it>
2023-02-16 10:58:44 +0100

    test: add ko test cases PAGOPA-653

--- src/test/java/it/pagopa/pagopa/apiconfig/service/RefreshServiceTest.java
+++ src/test/java/it/pagopa/pagopa/apiconfig/service/RefreshServiceTest.java
@@ -5,6 +5,7 @@ import feign.mock.HttpMethod;
 import feign.mock.MockClient;
 import feign.mock.MockTarget;
 import it.pagopa.pagopa.apiconfig.ApiConfig;
+import it.pagopa.pagopa.apiconfig.exception.AppException;
 import it.pagopa.pagopa.apiconfig.model.ConfigurationDomain;
 import it.pagopa.pagopa.apiconfig.model.JobTrigger;
 import it.pagopa.pagopa.apiconfig.util.RefreshClient;
@@ -13,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest(classes = ApiConfig.class)
 public class RefreshServiceTest {
@@ -20,18 +22,19 @@ public class RefreshServiceTest {
     private RefreshService refreshService;
     private MockClient mockClient;
 
-      @BeforeEach
-      void setUp() {
-          mockClient = new MockClient()
-              .ok(HttpMethod.GET, "/jobs/trigger/" + JobTrigger.PA_INVIA_RT.getValue(), "SUCCESS")
-              .ok(HttpMethod.GET, "/jobs/trigger/" + JobTrigger.REFRESH_CONFIGURATION.getValue(), "SUCCESS")
-              .ok(HttpMethod.GET, "/config/refresh/" + ConfigurationDomain.FTP_SERVER.getValue(), "SUCCESS");
+    @BeforeEach
+    void setUp() {
+        mockClient = new MockClient()
+            .ok(HttpMethod.GET, "/jobs/trigger/" + JobTrigger.PA_INVIA_RT.getValue(), "SUCCESS")
+            .ok(HttpMethod.GET, "/jobs/trigger/" + JobTrigger.REFRESH_CONFIGURATION.getValue(), "SUCCESS")
+            .ok(HttpMethod.GET, "/config/refresh/" + ConfigurationDomain.FTP_SERVER.getValue(), "SUCCESS")
+            .add(HttpMethod.GET, "/config/refresh/" + ConfigurationDomain.PA.getValue(), 500) // simulates a server-side error
+            .add(HttpMethod.GET, "/jobs/trigger/" + JobTrigger.PA_RETRY_PA_INVIA_RT_NEGATIVE.getValue(), 500);
 
-          RefreshClient refreshClient = Feign.builder()
-              .client(mockClient)
-              .target(new MockTarget<>(RefreshClient.class));
-
-          refreshService.setClient(refreshClient);
+        RefreshClient refreshClient = Feign.builder()
+            .client(mockClient)
+            .target(new MockTarget<>(RefreshClient.class));
+        refreshService.setClient(refreshClient);
       }
 
     @Test
@@ -55,5 +58,19 @@ public class RefreshServiceTest {
         String response = refreshService.jobTrigger(JobTrigger.PA_INVIA_RT);
         mockClient.verifyOne(HttpMethod.GET, "/jobs/trigger/" + JobTrigger.PA_INVIA_RT.getValue());
         assertEquals("SUCCESS", response);
+    }
+
+    @Test
+    void jobTrigger_500() {
+        // simulates a server-side error
+        Throwable exception = assertThrows(AppException.class, () -> refreshService.jobTrigger(JobTrigger.PA_RETRY_PA_INVIA_RT_NEGATIVE));
+        assertEquals("Something was wrong", exception.getMessage());
+    }
+
+    @Test
+    void refreshDomainConfig_500() {
+        // simulates a server-side error
+        Throwable exception = assertThrows(AppException.class, () -> refreshService.refreshConfig(ConfigurationDomain.PA));
+        assertEquals("Something was wrong", exception.getMessage());
     }
 }
