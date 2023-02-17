f7a4071e4a368d3c242d0cfb1494cd7271d05d8c
Jacopo <jacopo.carlini@gmail.com>
2023-02-17 11:01:18 +0100

    fix

--- src/main/java/it/pagopa/pagopa/apiconfig/ApiConfig.java
+++ src/main/java/it/pagopa/pagopa/apiconfig/ApiConfig.java
@@ -4,12 +4,14 @@ import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
-//@EnableJpaRepositories("it.pagopa.pagopa.apiconfig.repository")
-//@EnableRetry
+@EnableJpaRepositories("it.pagopa.pagopa.apiconfig.repository")
+@EnableRetry
 public class ApiConfig {
 
     public static void main(String[] args) {
