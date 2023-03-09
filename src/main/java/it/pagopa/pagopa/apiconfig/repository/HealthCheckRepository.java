e53d88fd353b506cc754a2ce47a7d6db6a6de8e6
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-03-09 19:44:42 +0100

    wip

--- src/main/java/it/pagopa/pagopa/apiconfig/repository/HealthCheckRepository.java
+++ src/main/java/it/pagopa/pagopa/apiconfig/repository/HealthCheckRepository.java
@@ -1,11 +1,10 @@
 package it.pagopa.pagopa.apiconfig.repository;
 
+import java.util.Optional;
+import javax.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
-
-import javax.persistence.EntityManager;
-import java.util.Optional;
 
 @Repository
 public class HealthCheckRepository {
