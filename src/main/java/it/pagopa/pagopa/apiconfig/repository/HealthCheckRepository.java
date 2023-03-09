1ae07e53b1ebd7af8de70ba23d25045fb3a15a01
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-03-09 15:57:50 +0100

    fix query

--- src/main/java/it/pagopa/pagopa/apiconfig/repository/HealthCheckRepository.java
+++ src/main/java/it/pagopa/pagopa/apiconfig/repository/HealthCheckRepository.java
@@ -1,6 +1,7 @@
 package it.pagopa.pagopa.apiconfig.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -12,7 +13,10 @@ public class HealthCheckRepository {
     @Autowired
     EntityManager entityManager;
 
+    @Value("${healthcheck.query}")
+    private String query;
+
     public Optional<Object> health() {
-        return Optional.of(entityManager.createNativeQuery("select 1").getSingleResult());
+        return Optional.of(entityManager.createNativeQuery(query).getSingleResult());
     }
 }
