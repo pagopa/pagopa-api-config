88c1d9fe04c564b77d22fcea3e47384838d68bf4
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-03-21 10:44:25 +0100

    wip

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-exec java -javaagent:/applicationinsights-agent.jar ${JAVA_OPTS} org.springframework.boot.loader.JarLauncher "$@"
+#exec java -javaagent:/applicationinsights-agent.jar ${JAVA_OPTS} org.springframework.boot.loader.JarLauncher "$@"
+exec java ${JAVA_OPTS} org.springframework.boot.loader.JarLauncher "$@"
