d8eec7d551964d290977a55b414e1919b14f9903
Francesco Cesareo <cesareo.francesco@gmail.com>
2023-03-23 11:56:33 +0100

    fix flyway

---
+++ database/sql/V15__ALTER_STAZIONI.sql
@@ -0,0 +1,3 @@
+alter table NODO4_CFG.STAZIONI add TARGET_HOST_POF varchar(100);
+alter table NODO4_CFG.STAZIONI add TARGET_PORT_POF NUMBER;
+alter table NODO4_CFG.STAZIONI add TARGET_PATH_POF varchar(10);
---
+++ database/sql/V17__ALTER_CANALI.sql
@@ -0,0 +1,3 @@
+alter table NODO4_CFG.CANALI add TARGET_HOST_NMP varchar(100);
+alter table NODO4_CFG.CANALI add TARGET_PORT_NMP NUMBER;
+alter table NODO4_CFG.CANALI add TARGET_PATH_NMP varchar(100);
