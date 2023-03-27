c46fdd7b7429ab0f9a37a735c89e171d9ee970ca
Jacopo <jacopo.carlini@gmail.com>
2023-03-27 18:29:09 +0200

    [PAGOPA-681] provider

--- .identity/00_main.tf
+++ .identity/00_main.tf
@@ -14,6 +14,10 @@ terraform {
       source  = "integrations/github"
       version = "5.12.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "= 2.18.1"
+    }
   }
 
   backend "azurerm" {}
@@ -33,3 +37,6 @@ data "azurerm_subscription" "current" {}
 
 data "azurerm_client_config" "current" {}
 
+provider "kubernetes" {
+  config_path = "${var.k8s_kube_config_path_prefix}/config-${local.aks_name}"
+}
