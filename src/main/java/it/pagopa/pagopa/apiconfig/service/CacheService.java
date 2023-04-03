47aec91aabae51ac6e9da115745f64835a5746bc
aacitelli <aacitelli@host.docker.internal>
2023-04-03 15:15:50 +0200

    [PAGOPA-834] import apiconfig starter lib: removed useless version properties in pom

--- src/main/java/it/pagopa/pagopa/apiconfig/service/CacheService.java
+++ src/main/java/it/pagopa/pagopa/apiconfig/service/CacheService.java
@@ -1,6 +1,10 @@
 package it.pagopa.pagopa.apiconfig.service;
 
+import java.lang.reflect.Type;
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -37,9 +41,11 @@ public class CacheService {
     Pageable pageable = PageRequest.of(page, limit, sortOrder);
     Page<Cache> data =
         cacheRepository.findAll(pageable);
-
+    
+    Type listType = new TypeToken<List<it.pagopa.pagopa.apiconfig.model.configuration.Cache>>(){}.getType();
+    
     return CacheVersions.builder()
-        .versionList(data.toList())
+        .versionList(modelMapper.map(data.toList(), listType))
         .pageInfo(CommonUtil.buildPageInfo(data))
         .build();
   }
