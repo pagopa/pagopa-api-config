9064dbacc7f382821b0a397c910f72c618f8d026
AngeloCaporaso <angelo.caporaso@pagopa.it>
2023-06-09 18:16:55 +0200

    [PAGOPA-987] feat: Add GET enhanced ibans

---
+++ src/main/java/it/gov/pagopa/apiconfig/core/model/creditorinstitution/IbansV2.java
@@ -0,0 +1,28 @@
+package it.gov.pagopa.apiconfig.core.model.creditorinstitution;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/** Ibans */
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class IbansV2 {
+
+  @JsonProperty("ibans")
+  @Schema(required = true)
+  @NotNull
+  @Valid
+  private List<IbanV2> ibanV2List;
+}
