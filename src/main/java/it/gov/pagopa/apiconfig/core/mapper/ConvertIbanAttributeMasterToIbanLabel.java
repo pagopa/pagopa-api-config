cb1e92d748f0d3b8519dff97835a21a736d74947
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-06-12 12:52:37 +0200

    [PAGOPA-989] feat: added mapper for convertion from IbanAttributeMaster to IbanLabel

---
+++ src/main/java/it/gov/pagopa/apiconfig/core/mapper/ConvertIbanAttributeMasterToIbanLabel.java
@@ -0,0 +1,21 @@
+package it.gov.pagopa.apiconfig.core.mapper;
+
+import it.gov.pagopa.apiconfig.core.model.creditorinstitution.IbanLabel;
+import it.gov.pagopa.apiconfig.starter.entity.IbanAttribute;
+import it.gov.pagopa.apiconfig.starter.entity.IbanAttributeMaster;
+import org.modelmapper.Converter;
+import org.modelmapper.spi.MappingContext;
+
+import javax.validation.Valid;
+
+public class ConvertIbanAttributeMasterToIbanLabel implements Converter<IbanAttributeMaster, IbanLabel> {
+
+  @Override
+  public IbanLabel convert(MappingContext<IbanAttributeMaster, IbanLabel> context) {
+    @Valid IbanAttribute source = context.getSource().getIbanAttribute();
+    return IbanLabel.builder()
+        .name(source.getAttributeName())
+        .description(source.getAttributeDescription())
+        .build();
+  }
+}
