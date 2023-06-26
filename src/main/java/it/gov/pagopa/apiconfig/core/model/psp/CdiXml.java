ebc26b56d140c98d06784f41d5114bd3e7b4c9ee
Jacopo <jacopo.carlini@gmail.com>
2023-06-26 12:08:50 +0200

    [PAGOPA-867] fix

--- src/main/java/it/gov/pagopa/apiconfig/core/model/psp/CdiXml.java
+++ src/main/java/it/gov/pagopa/apiconfig/core/model/psp/CdiXml.java
@@ -4,12 +4,7 @@ import it.gov.pagopa.apiconfig.core.util.XMLDateAdapter;
 import java.time.LocalDateTime;
 import java.util.List;
 import javax.validation.constraints.NotNull;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
-import javax.xml.bind.annotation.XmlElements;
-import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.*;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import lombok.*;
 
