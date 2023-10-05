f9cd7b765c8f912670948717fb6ff0df91354471
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-10-05 16:43:42 +0200

    PAGOPA-1239 inverting publication and validity date

--- scripts/utils/pythonExportIban/exportingIbanMastertoDB.py
+++ scripts/utils/pythonExportIban/exportingIbanMastertoDB.py
@@ -16,7 +16,7 @@ with open('./IbanCsv/Iban_Master_output.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     next(csv_reader)
     for row in csv_reader:
-        cursor.execute(f"INSERT INTO NODO4_CFG.iban_master (inserted_date, validity_date, state, fk_pa, fk_iban, description) VALUES (to_timestamp('{row[4]}','YYYY-MM-DD HH24:MI:SS'), to_timestamp('{row[5]}','YYYY-MM-DD HH24:MI:SS'), '{row[3]}', (Select obj_id from NODO4_CFG.pa where id_dominio = '{row[1]}'), (Select obj_id from NODO4_CFG.iban where iban = '{row[2]}'), '{row[6]}')")
+        cursor.execute(f"INSERT INTO NODO4_CFG.iban_master (inserted_date, validity_date, state, fk_pa, fk_iban, description) VALUES (to_timestamp('{row[5]}','YYYY-MM-DD HH24:MI:SS'), to_timestamp('{row[4]}','YYYY-MM-DD HH24:MI:SS'), '{row[3]}', (Select obj_id from NODO4_CFG.pa where id_dominio = '{row[1]}'), (Select obj_id from NODO4_CFG.iban where iban = '{row[2]}'), '{row[6]}')")
 
 cursor.close()
 connection.close()
