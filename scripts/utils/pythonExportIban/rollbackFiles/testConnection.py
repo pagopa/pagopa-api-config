cf0c37ab0c254c8968d89c66cb8d89f502dea2f6
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-10-11 11:22:53 +0200

    PAGOPA-1239 adding test connection to db

---
+++ scripts/utils/pythonExportIban/rollbackFiles/testConnection.py
@@ -0,0 +1,18 @@
+import jaydebeapi
+import sys
+
+host = "jdbc:oracle:thin:@" + sys.argv[1] + ":" + sys.argv[2] + "/" + sys.argv[3]
+username = sys.argv[4]
+password = sys.argv[5]
+driver = sys.argv[6]
+connection  = jaydebeapi.connect(
+    "oracle.jdbc.driver.OracleDriver",
+    host,
+    [username, password],
+    driver)
+cursor = connection.cursor()
+cursor.execute("select count(*) from iban")
+count = cursor.fetchall()
+print(count)
+cursor.close()
+connection.close()
