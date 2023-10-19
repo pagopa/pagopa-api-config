095c9ce3f7628f27a51373a999f536bb1759eb96
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-10-19 10:14:25 +0200

    PAGOPA-1239 fixing iban master to db

--- scripts/utils/pythonExportIban/exportingIbanMastertoDB.py
+++ scripts/utils/pythonExportIban/exportingIbanMastertoDB.py
@@ -10,11 +10,12 @@ connection = oracledb.connect(
     password=os.environ['SPRING_DATASOURCE_PASSWORD']
 )
 cursor = connection.cursor()
+data = []
 with open('./IbanCsv/Iban_Master_output.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    next(csv_reader)
-    for row in csv_reader:
-        cursor.execute(f"INSERT INTO NODO4_CFG.iban_master (inserted_date, validity_date, state, fk_pa, fk_iban, description) VALUES (to_timestamp('{row[5]}','YYYY-MM-DD HH24:MI:SS'), to_timestamp('{row[4]}','YYYY-MM-DD HH24:MI:SS'), '{row[3]}', (Select obj_id from NODO4_CFG.pa where id_dominio = '{row[1]}'), (Select obj_id from NODO4_CFG.iban where iban = '{row[2]}'), '{row[6]}')")
-
+    data = list(csv.reader(csv_file))
+cursor.executemany("""
+        INSERT INTO NODO4_CFG.iban_master (inserted_date, validity_date, state, fk_pa, fk_iban, description)
+        VALUES (to_timestamp(:6,'YYYY-MM-DD HH24:MI:SS'), to_timestamp(:5,'YYYY-MM-DD HH24:MI:SS'), :4, (Select obj_id from NODO4_CFG.pa where id_dominio = :2), (Select obj_id from NODO4_CFG.iban where iban = :3), :7)""", data)
+connection.commit()
 cursor.close()
 connection.close()
--- scripts/utils/pythonExportIban/exportingIbanToDB.py
+++ scripts/utils/pythonExportIban/exportingIbanToDB.py
@@ -13,7 +13,6 @@ cursor = connection.cursor()
 data = []
 with open('./IbanCsv/Iban_output.csv') as csv_file:
     data = list(csv.reader(csv_file))
-print(data)
 cursor.executemany("""
         INSERT INTO NODO4_CFG.IBAN (IBAN, FISCAL_CODE, DESCRIPTION, DUE_DATE)
         VALUES (':1', ':2', ':3', to_timestamp(':4','YYYY-MM-DD HH24:MI:SS.FF6'))""", data)
