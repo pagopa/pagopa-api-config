4b9498470d30c3987c6d526ae3304bf1f54f4897
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-10-19 13:20:28 +0200

    PAGOPA-1239 fixing master order

--- scripts/utils/pythonExportIban/exportingIbanMastertoDB.py
+++ scripts/utils/pythonExportIban/exportingIbanMastertoDB.py
@@ -14,8 +14,8 @@ data = []
 with open('./IbanCsv/Iban_Master_output.csv') as csv_file:
     data = list(csv.reader(csv_file))
 cursor.executemany("""
-        INSERT INTO NODO4_CFG.iban_master (inserted_date, validity_date, state, fk_pa, fk_iban, description)
-        VALUES (to_timestamp(:6,'YYYY-MM-DD HH24:MI:SS'), to_timestamp(:5,'YYYY-MM-DD HH24:MI:SS'), :4, (Select obj_id from NODO4_CFG.pa where id_dominio = :2), (Select obj_id from NODO4_CFG.iban where iban = :3), :7)""", data)
+        INSERT INTO NODO4_CFG.iban_master (description, fk_pa, fk_iban, state, validity_date, inserted_date)
+        VALUES (:1, (Select obj_id from NODO4_CFG.pa where id_dominio = :2), (Select obj_id from NODO4_CFG.iban where iban = :3), :4,to_timestamp(:5,'YYYY-MM-DD HH24:MI:SS'), to_timestamp(:6,'YYYY-MM-DD HH24:MI:SS'))""", data)
 connection.commit()
 cursor.close()
 connection.close()
