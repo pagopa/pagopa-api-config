e6280f76d45a85be021ed4ea5eabb663a961a0ea
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-10-19 14:48:59 +0200

    PAGOPA-1239 minor fix

--- scripts/utils/pythonExportIban/exportingIbanAttributesToDB.py
+++ scripts/utils/pythonExportIban/exportingIbanAttributesToDB.py
@@ -23,12 +23,13 @@ with open(sys.argv[1]) as csv_file:
     for row in csv_reader:
         cursor.execute("""
         Select obj_id from NODO4_CFG.iban_master where fk_pa=(Select obj_id from NODO4_CFG.pa where id_Dominio=:id_dominio) and fk_iban=(Select obj_id from NODO4_CFG.iban where iban=:iban)
-        """, [row[1], row[3]])
+        """, id_dominio=row[1], iban=row[3])
         iban_master_element = cursor.fetchall()
         if(len(iban_master_element) == 1 and row[8] == '0201138TS'):
             data.append([iban_master_element[0][0], iban_attribute[0][0]])
 
 cursor.executemany("INSERT INTO NODO4_CFG.iban_attributes_master (FK_IBAN_MASTER, FK_IBAN_ATTRIBUTE) VALUES (:1, :2)", data)
+connection.commit()
 
 cursor.close()
 connection.close()
