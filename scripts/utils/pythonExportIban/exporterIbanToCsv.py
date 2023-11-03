456be599d4b6d918fe4df697505d391ee33ce5f4
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-11-03 09:31:38 +0100

    PAGOPA-1239 logic about one line

--- scripts/utils/pythonExportIban/exporterIbanToCsv.py
+++ scripts/utils/pythonExportIban/exporterIbanToCsv.py
@@ -9,16 +9,13 @@ with open('./IbanCsv/Iban_Master_output.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
-        if line_count == 0:
-            line_count += 1
-        else:
-            if(thisdictDate.get(row[2]) != None):
-                if(datetime.strptime(thisdictDate.get(row[2]), "%Y-%m-%d %H:%M:%S") > datetime.strptime((row[4]), "%Y-%m-%d %H:%M:%S")):
-                    thisdictDate.update({row[2]: row[4]})
-                    thisdictPa.update({row[2]: row[1]})
-            else:
+        if(thisdictDate.get(row[2]) != None):
+            if(datetime.strptime(thisdictDate.get(row[2]), "%Y-%m-%d %H:%M:%S") > datetime.strptime((row[4]), "%Y-%m-%d %H:%M:%S")):
                 thisdictDate.update({row[2]: row[4]})
                 thisdictPa.update({row[2]: row[1]})
+        else:
+            thisdictDate.update({row[2]: row[4]})
+            thisdictPa.update({row[2]: row[1]})
 
 
 with open('./IbanCsv/Iban_Master_output.csv', 'r', newline='') as source, open('./IbanCsv/Iban_output.csv', 'w', newline='') as result:
