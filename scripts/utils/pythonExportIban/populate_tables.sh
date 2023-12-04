113844b887d4257c87617e3a9baa938d7a4a17f8
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-12-04 16:28:23 +0100

    adding description

--- scripts/utils/pythonExportIban/populate_tables.sh
+++ scripts/utils/pythonExportIban/populate_tables.sh
@@ -4,31 +4,31 @@ PDA_CSV_FILE=$1
 
 echo "Create file where View will be saved"
 
-mkdir ./IbanCsv
+mkdir ./IbanCsv #crea la cartella per i file di pre process
 
-touch ./IbanCsv/IbanView.csv
+touch ./IbanCsv/IbanView.csv #crea il file csv con il contenuto della vista
 
-python3 exporterViewToCsv.py
+python3 exporterViewToCsv.py #esporta la vista all'interno del file appena creato
 
 echo "Original data from existing view exported"
 
-python3 exporterIbanMasterToCsv.py $PDA_CSV_FILE
+python3 exporterIbanMasterToCsv.py $PDA_CSV_FILE #preparazione del file csv iban master
 
-python3 exporterIbanToCsv.py
+python3 exporterIbanToCsv.py #preparazione del file csv per iban
 
 echo "Pre process done"
 
-python3 exportingIbanToDB.py
+python3 exportingIbanToDB.py # popolamento tabella iban
 
 echo "Populated Iban table"
 
-python3 exportingIbanMastertoDB.py
+python3 exportingIbanMastertoDB.py # popolamento tabella iban master
 
 echo "Populated Iban Master table"
 
-python3 exportingIbanAttributesToDB.py
+python3 exportingIbanAttributesToDB.py # popolamento tabella iban attribute
 
-python3 exportingIbanAttributesMasterToDB.py $PDA_CSV_FILE
+python3 exportingIbanAttributesMasterToDB.py $PDA_CSV_FILE # popolamento iban attribute master
 
 echo "Populated Iban Attributes and Iban Attributes Master table"
 
