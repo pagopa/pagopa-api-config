1129eabf4c1d9ab6009fb54ad57deecd662f4472
Jacopo <jacopo.carlini@gmail.com>
2024-01-24 15:09:01 +0100

    logging

--- src/main/java/it/gov/pagopa/apiconfig/core/config/LoggingAspect.java
+++ src/main/java/it/gov/pagopa/apiconfig/core/config/LoggingAspect.java
@@ -1,5 +1,7 @@
 package it.gov.pagopa.apiconfig.core.config;
 
+import it.gov.pagopa.apiconfig.core.exception.AppError;
+import it.gov.pagopa.apiconfig.core.model.ProblemJson;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -7,6 +9,7 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.CodeSignature;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,7 +26,12 @@ import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import java.util.stream.StreamSupport;
+
+import static it.gov.pagopa.apiconfig.core.util.CommonUtil.deNull;
 
 @Aspect
 @Component
@@ -35,6 +43,12 @@ public class LoggingAspect {
     public static final String STATUS = "status";
     public static final String CODE = "httpCode";
     public static final String RESPONSE_TIME = "responseTime";
+    public static final String FAULT_CODE = "faultCode";
+    public static final String FAULT_DETAIL = "faultDetail";
+    public static final String REQUEST_ID = "requestId";
+    public static final String OPERATION_ID = "operationId";
+    public static final String ARGS = "args";
+
     @Autowired
     HttpServletRequest httRequest;
     @Autowired
@@ -46,15 +60,6 @@ public class LoggingAspect {
     @Value("${info.properties.environment}")
     private String environment;
 
-    private static String getExecutionTime() {
-        String startTime = MDC.get(START_TIME);
-        if (startTime != null) {
-            long endTime = System.currentTimeMillis();
-            long executionTime = endTime - Long.parseLong(startTime);
-            return String.valueOf(executionTime);
-        }
-        return "1";
-    }
 
     @Pointcut("@within(org.springframework.web.bind.annotation.RestController)")
     public void restController() {
@@ -110,8 +115,15 @@ public class LoggingAspect {
     public Object logApiInvocation(ProceedingJoinPoint joinPoint) throws Throwable {
         MDC.put(METHOD, joinPoint.getSignature().getName());
         MDC.put(START_TIME, String.valueOf(System.currentTimeMillis()));
-        log.info("{} {}", httRequest.getMethod(), httRequest.getRequestURI());
-        log.info("Invoking API operation {} - args: {}", joinPoint.getSignature().getName(), joinPoint.getArgs());
+        MDC.put(OPERATION_ID, UUID.randomUUID().toString());
+        if(MDC.get(REQUEST_ID) == null) {
+            var requestId = UUID.randomUUID().toString();
+            MDC.put(REQUEST_ID, requestId);
+        }
+        Map<String, String> params = getParams(joinPoint);
+        MDC.put(ARGS, params.toString());
+
+        log.info("Invoking API operation {} - args: {}", joinPoint.getSignature().getName(), params);
 
         Object result = joinPoint.proceed();
 
@@ -127,22 +139,54 @@ public class LoggingAspect {
     }
 
     @AfterReturning(value = "execution(* *..exception.ErrorHandler.*(..))", returning = "result")
-    public void trowingApiInvocation(JoinPoint joinPoint, ResponseEntity<?> result) {
+    public void trowingApiInvocation(JoinPoint joinPoint, ResponseEntity<ProblemJson> result) {
         MDC.put(STATUS, "KO");
         MDC.put(CODE, String.valueOf(result.getStatusCodeValue()));
         MDC.put(RESPONSE_TIME, getExecutionTime());
+        MDC.put(FAULT_CODE, getTitle(result));
+        MDC.put(FAULT_DETAIL, getDetail(result));
         log.info("Failed API operation {} - error: {}", MDC.get(METHOD), result);
-        MDC.remove(STATUS);
-        MDC.remove(CODE);
-        MDC.remove(RESPONSE_TIME);
-        MDC.remove(START_TIME);
+        MDC.clear();
     }
 
     @Around(value = "repository() || service()")
     public Object logTrace(ProceedingJoinPoint joinPoint) throws Throwable {
-        log.debug("Call method {} - args: {}", joinPoint.getSignature().toShortString(), joinPoint.getArgs());
+        Map<String, String> params = getParams(joinPoint);
+        log.debug("Call method {} - args: {}", joinPoint.getSignature().toShortString(), params);
         Object result = joinPoint.proceed();
         log.debug("Return method {} - result: {}", joinPoint.getSignature().toShortString(), result);
         return result;
+    }
+
+    private static String getDetail(ResponseEntity<ProblemJson> result) {
+        if(result != null && result.getBody() != null && result.getBody().getDetail() != null) {
+            return result.getBody().getDetail();
+        } else return AppError.UNKNOWN.getDetails();
+    }
+
+    private static String getTitle(ResponseEntity<ProblemJson> result) {
+        if(result != null && result.getBody() != null && result.getBody().getTitle() != null) {
+            return result.getBody().getTitle();
+        } else return AppError.UNKNOWN.getTitle();
+    }
+
+    private static String getExecutionTime() {
+        String startTime = MDC.get(START_TIME);
+        if (startTime != null) {
+            long endTime = System.currentTimeMillis();
+            long executionTime = endTime - Long.parseLong(startTime);
+            return String.valueOf(executionTime);
+        }
+        return "-";
+    }
+
+    private static Map<String, String> getParams(ProceedingJoinPoint joinPoint) {
+        CodeSignature codeSignature = (CodeSignature) joinPoint.getSignature();
+        Map<String, String> params = new HashMap<>();
+        int i = 0;
+        for (var paramName : codeSignature.getParameterNames()) {
+            params.put(paramName, deNull(joinPoint.getArgs()[i++]));
+        }
+        return params;
     }
 }
