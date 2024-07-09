e54d3b514c12e40d1fe3aea7309de4fe605bec0c
svariant <samuele.varianti@nttdata.com>
2024-07-09 10:46:32 +0200

    [VAS-1146] feat: Add primitiveVersion mapping to mapper Canali-Channel

--- src/main/java/it/gov/pagopa/apiconfig/core/mapper/ConvertCanaliToChannel.java
+++ src/main/java/it/gov/pagopa/apiconfig/core/mapper/ConvertCanaliToChannel.java
@@ -3,7 +3,9 @@ package it.gov.pagopa.apiconfig.core.mapper;
 import it.gov.pagopa.apiconfig.core.model.psp.Channel;
 import it.gov.pagopa.apiconfig.core.util.CommonUtil;
 import it.gov.pagopa.apiconfig.starter.entity.Canali;
+
 import javax.validation.Valid;
+
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.Converter;
 import org.modelmapper.spi.MappingContext;
@@ -11,15 +13,16 @@ import org.modelmapper.spi.MappingContext;
 @Slf4j
 public class ConvertCanaliToChannel implements Converter<Canali, Channel> {
 
-  @Override
-  public Channel convert(MappingContext<Canali, Channel> context) {
-    @Valid Canali source = context.getSource();
-    log.debug(source.toString());
-    return Channel.builder()
-        .channelCode(source.getIdCanale())
-        .enabled(source.getEnabled())
-        .brokerDescription(
-            CommonUtil.deNull(source.getFkIntermediarioPsp().getCodiceIntermediario()))
-        .build();
-  }
+    @Override
+    public Channel convert(MappingContext<Canali, Channel> context) {
+        @Valid Canali source = context.getSource();
+        log.debug(source.toString());
+        return Channel.builder()
+                .channelCode(source.getIdCanale())
+                .enabled(source.getEnabled())
+                .primitiveVersion(source.getFkCanaliNodo() != null ? source.getFkCanaliNodo().getVersionePrimitive() : null)
+                .brokerDescription(
+                        CommonUtil.deNull(source.getFkIntermediarioPsp().getCodiceIntermediario()))
+                .build();
+    }
 }
