e33f164b385f01c9bac6fc1e045bebfd5738f534
giomella <giomella@emeal.nttdata.com>
2024-07-19 11:26:56 +0200

    [VAS-1171] defined maintenance models

--- src/main/java/it/gov/pagopa/apiconfig/core/model/stationmaintenance/CreateStationMaintenance.java
+++ src/main/java/it/gov/pagopa/apiconfig/core/model/stationmaintenance/CreateStationMaintenance.java
@@ -1,12 +1,17 @@
 package it.gov.pagopa.apiconfig.core.model.stationmaintenance;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.swagger.v3.oas.annotations.media.Schema;
+import it.gov.pagopa.apiconfig.core.util.Constants;
+import it.gov.pagopa.apiconfig.core.util.OffsetDateTimeDeserializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotNull;
 import java.time.OffsetDateTime;
@@ -22,22 +27,34 @@ import java.time.OffsetDateTime;
 public class CreateStationMaintenance {
 
     @JsonProperty("start_date_time")
-    @Schema(required = true)
     @NotNull
+    @JsonFormat(pattern = Constants.DateTimeFormat.DATE_TIME_FORMAT)
+    @JsonDeserialize(using = OffsetDateTimeDeserializer.class)
+    @Schema(
+            example = "2024-04-01T13:00:00.000Z",
+            required = true,
+            description = "The start date time of the station maintenance")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private OffsetDateTime startDateTime;
 
     @JsonProperty("end_date_time")
-    @Schema(required = true)
     @NotNull
+    @JsonFormat(pattern = Constants.DateTimeFormat.DATE_TIME_FORMAT)
+    @JsonDeserialize(using = OffsetDateTimeDeserializer.class)
+    @Schema(
+            example = "2024-04-01T13:00:00.000Z",
+            required = true,
+            description = "The end date time of the station maintenance")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private OffsetDateTime endDateTime;
 
     @JsonProperty("stand_in")
-    @Schema(required = true)
+    @Schema(description = "StandIn flag", required = true)
     @NotNull
     private Boolean standIn;
 
     @JsonProperty("station_code")
-    @Schema(required = true)
+    @Schema(description = "Code of the station subject of the maintenance", required = true)
     @NotNull
     private String stationCode;
 }
