46e11b1594025f24cf9a958e9c0adfe3db29ab39
Federico Ruzzier <federicoruzzier96@gmail.com>
2025-05-07 17:40:22 +0200

    chore: typo in identity name PIDM-487

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -52,7 +52,7 @@ data "azurerm_key_vault_secret" "key_vault_integration_test_subkey" {
 }
 
 data "azurerm_user_assigned_identity" "workload_identity_clientid" {
-  name                = "api-config-workload-identity"
+  name                = "apiconfig-workload-identity"
   resource_group_name = "pagopa-${var.env_short}-${local.location_short}-${var.env}-aks-rg"
 }
 
