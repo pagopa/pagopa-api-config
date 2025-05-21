8f027cc4a6d09477bcf4f6df3400812197bd7d51
Federico Ruzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2025-05-21 08:51:22 +0200

    Pidm 487 wl identity helm 7 5 0 (#334)
    * chore: adding new identity terraform elements PIDM-487
    
    * chore: update helm chart and files PIDM-487
    
    * chore: github actions PIDM-487
    
    * chore: new version azuread azurerm PIDM-487
    
    * chore: typo in identity name PIDM-487
    
    * chore: remove unused var from helm PIDM-487
    
    * chore: try fix action PIDM-487
    
    * chore: try fix action PIDM-487
    
    * Bump to version 0.59.28-1-PIDM-487-wl-identity-helm-7-5-0 [skip ci]
    
    * chore: fix action env PIDM-487
    
    * Bump to version 0.59.28-2-PIDM-487-wl-identity-helm-7-5-0 [skip ci]
    
    * Bump to version 0.59.28-3-PIDM-487-wl-identity-helm-7-5-0 [skip ci]
    
    * chore: wl id secret to variable PIDM-487
    
    * chore: fix helm value wl identity PIDM-487
    
    * Bump to version 0.59.28-4-PIDM-487-wl-identity-helm-7-5-0 [skip ci]
    
    * Bump to version 0.59.28-5-PIDM-487-wl-identity-helm-7-5-0 [skip ci]
    
    * Bump to version 0.59.28-6-PIDM-487-wl-identity-helm-7-5-0 [skip ci]
    
    * Bump to version 0.59.28-7-PIDM-487-wl-identity-helm-7-5-0 [skip ci]
    
    * Bump to version 0.59.28-8-PIDM-487-wl-identity-helm-7-5-0 [skip ci]
    
    * chore: fix action workload set PIDM-487
    
    * Bump to version 0.59.28-9-PIDM-487-wl-identity-helm-7-5-0 [skip ci]
    
    * chore: fix typo in action PIDM-487
    
    * Bump to version 0.59.28-10-PIDM-487-wl-identity-helm-7-5-0 [skip ci]
    
    ---------
    
    Co-authored-by: pagopa-github-bot <github-bot@pagopa.it>

--- .identity/99_main.tf
+++ .identity/99_main.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.30.0"
+      version = "~> 2.53"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.45.0"
+      version = "~> 3.117"
     }
     github = {
       source  = "integrations/github"
