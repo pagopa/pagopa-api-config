465d8cc10ba9ba38d88f44b2f9e8c145de765d71
RiccardoGiuliani <riccardo.giuliani@nttdata.com>
2026-02-05 11:09:31 +0100

    PIDM-1076 fix some smoke test

--- integration-test/src/step_definitions/support/iban.massive.js
+++ integration-test/src/step_definitions/support/iban.massive.js
@@ -2,87 +2,98 @@ const fs = require("fs");
 const assert = require("assert");
 const {When, Then, BeforeAll, AfterAll} = require('@cucumber/cucumber')
 const {createMassiveIbansByCsv} = require("./logic/ibanLogic")
+const {replaceDatesInCsv} = require("./utility/helpers")
 
 let body;
 let responseToCheck;
 
 // preventive cancellation to avoid dirty cases
-BeforeAll(async function() {
-        body = fs.readFileSync("step_definitions/support/resources/delete.csv", "utf-8");
-		await createMassiveIbansByCsv(body);
+BeforeAll(async function () {
+    body = fs.readFileSync("step_definitions/support/resources/delete.csv", "utf-8");
+    await createMassiveIbansByCsv(body);
 });
 
 // cleans the database after the integration tests
-AfterAll(async function() {
-        body = fs.readFileSync("step_definitions/support/resources/delete.csv", "utf-8");
-		await createMassiveIbansByCsv(body);
+AfterAll(async function () {
+    body = fs.readFileSync("step_definitions/support/resources/delete.csv", "utf-8");
+    await createMassiveIbansByCsv(body);
 });
 
 
 // massive create ibans
 When('the client loads a csv file with ibans to create', async () => {
-	    body = fs.readFileSync("step_definitions/support/resources/insert.csv", "utf-8");
-		responseToCheck = await createMassiveIbansByCsv(body);
+    body = fs.readFileSync("step_definitions/support/resources/insert.csv", "utf-8");
+    body = replaceDatesInCsv(body);
+    responseToCheck = await createMassiveIbansByCsv(body);
 });
 
 // massive update ibans 
 When('the client loads a csv file with ibans to update', async () => {
-	    body = fs.readFileSync("step_definitions/support/resources/update.csv", "utf-8");
-		responseToCheck = await createMassiveIbansByCsv(body);
+    body = fs.readFileSync("step_definitions/support/resources/update.csv", "utf-8");
+    body = replaceDatesInCsv(body);
+    responseToCheck = await createMassiveIbansByCsv(body);
 });
 
 // massive delete ibans 
 When('the client loads a csv file with ibans to delete', async () => {
-	    body = fs.readFileSync("step_definitions/support/resources/delete.csv", "utf-8");
-		responseToCheck = await createMassiveIbansByCsv(body);
+    body = fs.readFileSync("step_definitions/support/resources/delete.csv", "utf-8");
+    body = replaceDatesInCsv(body);
+    responseToCheck = await createMassiveIbansByCsv(body);
 });
 
 // mixed behavior
 When('the client loads a csv file with mixed behavior ibans', async () => {
-	    body = fs.readFileSync("step_definitions/support/resources/mix.csv", "utf-8");
-		responseToCheck = await createMassiveIbansByCsv(body);
+    body = fs.readFileSync("step_definitions/support/resources/mix.csv", "utf-8");
+    body = replaceDatesInCsv(body);
+    responseToCheck = await createMassiveIbansByCsv(body);
 });
 
 // BAD insert: two entries on the same PA for the same IBAN
 When('the client loads a bad csv file with two entries on the same PA for the same IBAN', async () => {
-	    body = fs.readFileSync("step_definitions/support/resources/BAD_insert.csv", "utf-8");
-		responseToCheck = await createMassiveIbansByCsv(body);
+    body = fs.readFileSync("step_definitions/support/resources/BAD_insert.csv", "utf-8");
+    body = replaceDatesInCsv(body);
+    responseToCheck = await createMassiveIbansByCsv(body);
 });
 
 // BAD insert: two insert operations for the same POSTAL IBAN in two different PAs
 When('the client loads a bad csv file with two insert operations for the same POSTAL IBAN in two different PAs', async () => {
-	    body = fs.readFileSync("step_definitions/support/resources/BAD_insert_postal_iban.csv", "utf-8");
-		responseToCheck = await createMassiveIbansByCsv(body);
+    body = fs.readFileSync("step_definitions/support/resources/BAD_insert_postal_iban.csv", "utf-8");
+    body = replaceDatesInCsv(body);
+    responseToCheck = await createMassiveIbansByCsv(body);
 });
 
 // BAD insert: invalid IBAN entry
 When('the client loads a bad csv file with an invalid IBAN entry', async () => {
-	    body = fs.readFileSync("step_definitions/support/resources/BAD_iban.csv", "utf-8");
-		responseToCheck = await createMassiveIbansByCsv(body);
+    body = fs.readFileSync("step_definitions/support/resources/BAD_iban.csv", "utf-8");
+    body = replaceDatesInCsv(body);
+    responseToCheck = await createMassiveIbansByCsv(body);
 });
 
 // BAD insert: csv file is incorrectly loaded twice in a row
 When('the client loads the same CSV file used previously', async () => {
-	    body = fs.readFileSync("step_definitions/support/resources/insert.csv", "utf-8");
-		responseToCheck = await createMassiveIbansByCsv(body);
+    body = fs.readFileSync("step_definitions/support/resources/insert.csv", "utf-8");
+    body = replaceDatesInCsv(body);
+    responseToCheck = await createMassiveIbansByCsv(body);
 });
 
 // BAD delete: PA-Iban relationship does not exist
 When('the client loads a bad csv file with PA - Iban relationship that does not exist for deletion', async () => {
-	    body = fs.readFileSync("step_definitions/support/resources/BAD_delete.csv", "utf-8");
-		responseToCheck = await createMassiveIbansByCsv(body);
+    body = fs.readFileSync("step_definitions/support/resources/BAD_delete.csv", "utf-8");
+    body = replaceDatesInCsv(body);
+    responseToCheck = await createMassiveIbansByCsv(body);
 });
 
 // BAD update: PA-Iban relationship does not exist
 When('the client loads a bad csv file with PA - Iban relationship that does not exist for the update', async () => {
-	    body = fs.readFileSync("step_definitions/support/resources/BAD_update.csv", "utf-8");
-		responseToCheck = await createMassiveIbansByCsv(body);
+    body = fs.readFileSync("step_definitions/support/resources/BAD_update.csv", "utf-8");
+    body = replaceDatesInCsv(body);
+    responseToCheck = await createMassiveIbansByCsv(body);
 });
 
 // BAD csv file: not well formed
 When('the client loads not well formed csv file', async () => {
-	    body = fs.readFileSync("step_definitions/support/resources/BAD_format.csv", "utf-8");
-		responseToCheck = await createMassiveIbansByCsv(body);
+    body = fs.readFileSync("step_definitions/support/resources/BAD_format.csv", "utf-8");
+    responseToCheck = await createMassiveIbansByCsv(body);
 });
 
 // check status code
--- integration-test/src/step_definitions/support/utility/helpers.js
+++ integration-test/src/step_definitions/support/utility/helpers.js
@@ -1,24 +1,56 @@
 function makeIdMix(length) {
-  var result           = '';
-  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  var charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
 }
 
 function makeIdNumber(length) {
-  var result           = '';
-  var characters       = '0123456789';
-  var charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
- return result;
+    let result = '';
+    let characters = '0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+function getFutureDate(daysFromNow = 30) {
+    const today = new Date();
+    const futureDate = new Date(today);
+    futureDate.setDate(today.getDate() + daysFromNow);
+
+    const year = futureDate.getFullYear();
+    const month = String(futureDate.getMonth() + 1).padStart(2, '0');
+    const day = String(futureDate.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+}
+
+function replaceDatesInCsv(csvContent) {
+    const dateRegex = /\d{4}-\d{2}-\d{2}/g;
+
+    const dateMap = new Map();
+
+    return csvContent.replace(dateRegex, (matchedDate) => {
+        if (dateMap.has(matchedDate)) {
+            return dateMap.get(matchedDate);
+        }
+
+        const dayOffset = dateMap.size * 5 + 30;
+        const newDate = getFutureDate(dayOffset);
+        dateMap.set(matchedDate, newDate);
+
+        return newDate;
+    });
 }
 
 module.exports = {
-  makeIdMix,
-  makeIdNumber
+    makeIdMix,
+    makeIdNumber,
+    getFutureDate,
+    replaceDatesInCsv
 }
